f915816825e872da5989c8b600ae1c9f53829dab
Alek Hurst <123alekhurst@gmail.com>
2014-10-28 21:05:29 -0700

    initial addition of data to dropdowns

--- js/controller.js
+++ js/controller.js
@@ -9,24 +9,19 @@ var CleanSlateApp = angular.module('CatalystApp', []);
 CleanSlateApp.controller('CatalystController', function ($scope) {
 
 	/* Scope Variable Declaration */
-	$scope.story_creation_input = {},
-	$scope.current_view = 'featured'
-
+	$scope.story_creation_input = {};
+	$scope.current_view = '';
+	$scope.card_types = [];
+	$scope.locations = [];
+	$scope.groups = [];
 
 	/* Scope Variable Initialization */
-	$scope.story_creation_input = {
-		first_name : '',
-		last_name : '',
-		email : '',
-		region : '',
-		group : '',
-		catalyst_commitment : '',
-		story : '',
-		photo_url : ''
-	},
-
-	$scope.possible_views = ['featured', 'all', 'map', 'create', 'admin'],
-
+	$scope.story_creation_input = { first_name : '', last_name : '', email : '', region : '', group : '', catalyst_commitment : '', story : '', photo_url : '' };
+	$scope.possible_views = ['featured', 'all', 'map', 'create', 'admin'];
+	$scope.current_view = 'featured',
+	$scope.card_types = window.catalyst_objects.card_types;
+	$scope.locations = window.catalyst_objects.locations;
+	$scope.groups = window.catalyst_objects.groups;
 
 	/**
 	 * Called when a user clicks "submit" after creating a Catalyst Story.
@@ -34,15 +29,16 @@ CleanSlateApp.controller('CatalystController', function ($scope) {
 	 * function calls an AJAX function to do the creation in the database.
 	 */
 	$scope.createStoryInDatabase = function() {
+		console.log($scope.story_creation_input)
 		$.ajax({
-		    url : "create_story.php",
+		    url : "server/create_story.php",
 		    type: "POST",
 		    data : $scope.story_creation_input,
 		    success: function(data) {
 		        $scope.current_view ='successful_story_creation'
 		    }
 		});
-	}
+	};
 
 	/**
 	 * Called whenever a user clicks on a navigation component or the create
@@ -50,5 +46,5 @@ CleanSlateApp.controller('CatalystController', function ($scope) {
 	 */
 	$scope.changeViews = function(view) {
 		$scope.current_view = view;
-	}
+	};
 });
---
+++ js/objects.js
@@ -0,0 +1,27 @@
+window.catalyst_objects = {
+	groups : [
+		{ id: 0000, title: 'Total Company' }, 
+		{ id: 0001, title: 'ProdOps' }, 
+		{ id: 0002, title: 'HR' }, 
+		{ id: 0003, title: 'Corp Strategy' }, 
+		{ id: 0004, title: 'Finance/IT/WPR' }, 
+		{ id: 0005, title: 'Legal' }, 
+		{ id: 0006, title: 'Go-To-Market' }, 
+		{ id: 0007, title: 'Marketing' }, 
+		{ id: 0008, title: 'FieldOps' }, 
+		{ id: 0009, title: 'CSOps' }, 
+		{ id: 0010, title: 'GTMOps' }
+	],
+	locations : [
+		{ id: 0000, title: 'APAC' }, 
+		{ id: 0001, title: 'India' },
+		{ id: 0002, title: 'EMEA' }, 
+		{ id: 0003, title: 'Americas' }
+	],
+	card_types : [
+		{ id: 0000, title: 'Candor'}, 
+		{ id: 0001, title: 'Clarity'}, 
+		{ id: 0002, title: 'Owndership'}, 
+		{ id: 0003, title: 'Speed' }
+	]
+}
