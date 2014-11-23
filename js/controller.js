39545ea51d59b497df1023cd6fcaf5f6bb83dae0
Alek Hurst <123alekhurst@gmail.com>
2014-11-22 21:18:31 -0800

    reverse

--- js/controller.js
+++ js/controller.js
@@ -335,4 +335,13 @@ CatalystApp.controller('CatalystController', function ($scope) {
 	}
 
 	$scope.initializeScopeVariables(); // called when controller code is finished loading
+})
+
+CatalystApp.filter('reverse', function() {
+  return function(items) {
+    if(typeof items === 'undefined') { return; }
+    return angular.isArray(items) ? 
+      items.slice().reverse() : // If it is an array, split and reverse it
+      (items + '').split('').reverse().join(''); // else make it a string (if it isn't already), and reverse it
+  };
 });
